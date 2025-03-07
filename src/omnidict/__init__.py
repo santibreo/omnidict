f29c464341d64fec1035606d5caaf4935bed34c9
Prophet Jeremy <santibreo@gmail.com>
2025-03-07 18:32:58 +0100

    fix: Package versioning was broken

--- src/omnidict/__init__.py
+++ src/omnidict/__init__.py
@@ -2,14 +2,15 @@
 
 import sys
 import subprocess
+from packaging.version import Version
 
 
 def _get_version_from_git_tags(default='v0.0.1-alpha'):
     cmd = subprocess.run(['git', 'tag'], capture_output=True)
-    try:
-        return next(map(str.strip, cmd.stdout.decode().splitlines()), default)
-    except StopIteration:
+    versions = sorted(map(Version, cmd.stdout.decode().splitlines()))
+    if not versions:
         return default
+    return str(versions[-1])
 
 
 # If you do following line, function is called everytime package is imported
