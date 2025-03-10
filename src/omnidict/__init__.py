f25d58be9ecc2763a150bd7a007ad3d3f356c46e
Prophet Jeremy <santibreo@gmail.com>
2025-03-10 09:36:21 +0100

    fix: Update version when accessing it

--- src/omnidict/__init__.py
+++ src/omnidict/__init__.py
@@ -1,16 +1,30 @@
 """Defines package version dynamically"""
 
+import re
 import sys
 import subprocess
+from pathlib import Path
 from packaging.version import Version
 
 
-def _get_version_from_git_tags(default='v0.0.1-alpha'):
+_PATTERN = re.compile(r"(def _get_version_from_git_tags\(default: str = )'.*'(\):)")
+
+
+def _get_version_from_git_tags(default: str = '1.0.0a3'):
+
+    def set_version_as_default(version: str):
+        content = Path(__file__).read_text()
+        new_content = re.sub(_PATTERN, r'\1' + f"{version!r}" + r'\2', content)
+        Path(__file__).write_text(new_content)
+
     cmd = subprocess.run(['git', 'tag'], capture_output=True)
     versions = sorted(map(Version, cmd.stdout.decode().splitlines()))
     if not versions:
+        if not default:
+            raise SystemError('Cannot resolve package version')
         return default
-    return str(versions[-1])
+    set_version_as_default(version := str(versions[-1]))
+    return version
 
 
 # If you do following line, function is called everytime package is imported
